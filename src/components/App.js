0c1a86a8c16248c195a7f5d03545fd87fe820125
Chris Gradwohl <christophergradwohl@gmail.com>
2019-12-06 16:51:54 -0800

    baby layoutt

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 
 const App = () => (
-  <>
+  <div>
     <h1>hello creature...</h1>
     <Button className="radicalBtn">Secondary</Button>
-  </>
+  </div>
 );
 
 export default App;
